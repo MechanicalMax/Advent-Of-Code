cdf9b7162314e87787f9dd38e40841dc022f96b7
Mechanical Max <93453184+MechanicalMax@users.noreply.github.com>
2022-12-01 18:56:56 -0500

    Add Setup Windows Desktop Shortcut

---
+++ SetupShortcutsWindows/Open Today's AoC.bat
@@ -0,0 +1,5 @@
+@echo off
+
+title Open Today's AoC
+
+"C:\Users\maxsh\OneDrive\Documents\CodingProjects\Advent of Code\Scripts\python.exe" "C:\Users\maxsh\OneDrive\Documents\CodingProjects\Advent of Code\Advent-Of-Code\AoCHelpers\setup.py" --today
