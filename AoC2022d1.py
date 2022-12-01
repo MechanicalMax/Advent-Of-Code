f0e4aa53ebbff9a5a1b4ffdf3bbbb1e760388618
Mechanical Max <93453184+MechanicalMax@users.noreply.github.com>
2022-12-01 17:01:32 -0500

    Solve AoC2022d1

--- AoC2022d1.py
+++ AoC2022d1.py
@@ -1,9 +1,32 @@
 import AoCHelpers.setupUtilities.submition as submit
 import AoCHelpers.setupUtilities.getPuzzleObject as getPuzzle
-puz = getPuzzle.getPuzzleObject(__file__)
 from aocd import lines
 
-def getHighestCalCount():
+puz = getPuzzle.getPuzzleObject(__file__)
+
+def sumElements(arr):
+    sum = 0
+    for ele in arr:
+        sum += ele
+    return sum
+
+def _sumTopThreeCalCounts():
+    maxCounts = [0, 0, 0]
+    currentCount = 0
+    for line in lines:
+        if line:
+            currentCount += int(line)
+        else:
+            for i, max in enumerate(maxCounts):
+                if currentCount > max:
+                    maxCounts.insert(i, currentCount)
+                    maxCounts.pop()
+                    break
+            currentCount = 0    
+
+    return sumElements(maxCounts)
+
+def _getHighestCalCount():
     maxCount = 0
     currentCount = 0
     for line in lines:
@@ -16,11 +39,12 @@ def getHighestCalCount():
     return maxCount
 
 def partA(puz):
-    ans = getHighestCalCount()
+    ans = _getHighestCalCount()
     submit.safeSubmit(puz, ans, 'a')
 
 def partB(puz):
-    ans = 0
+    ans = _sumTopThreeCalCounts()
     submit.safeSubmit(puz, ans, 'b')
 
 partA(puz)
+partB(puz)
