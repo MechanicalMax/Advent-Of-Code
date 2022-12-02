8ae7525a9a55f7814fcfdd97cc302c83915f03d1
Mechanical Max <93453184+MechanicalMax@users.noreply.github.com>
2022-12-01 19:16:33 -0500

    Add timeout to today's AoC windows shortcut

--- SetupShortcutsWindows/Open Today's AoC.bat
+++ SetupShortcutsWindows/Open Today's AoC.bat
@@ -1,5 +1,7 @@
 @echo off
-
+Rem gives the window a title
 title Open Today's AoC
-
+Rem opens my venv python.exe then runs my AoC setup script with the today flag
 "C:\Users\maxsh\OneDrive\Documents\CodingProjects\Advent of Code\Scripts\python.exe" "C:\Users\maxsh\OneDrive\Documents\CodingProjects\Advent of Code\Advent-Of-Code\AoCHelpers\setup.py" --today
+Rem closes the file after waiting so the user can see the messages
+timeout /t 5
